e1b51f668f0fae93dd00aef2536b7d4ef950928e
fhassanpur <fhassanpur@gmail.com>
2015-01-17 23:50:34 -0800

    Basic collision detection and basic scoring system

--- game.py
+++ game.py
@@ -1,14 +1,86 @@
 import sys, pygame
+from objs import FPaddle, FBall
 
 class Game():
-
+	# USEREVENT+1 is left player score
+	# USEREVENT+2 is right player score
 
 	def __init__(self):
 		pass
 
 	def start(self):
 		pygame.init()
+		pygame.font.init()
 		width = 800
 		height = 600
 
-		self.screen = pygame.display.set_mode(width, height)
+		self.clock = pygame.time.Clock()
+		self.running = True
+
+		self.screen = pygame.display.set_mode((width, height))
+
+		# Creates objects
+		self.ball = FBall()
+		self.paddle_left = FPaddle()
+		self.paddle_left.set_player_left()
+		self.paddle_right = FPaddle()
+		self.paddle_right.set_player_right()
+
+		# Creates labels
+		self.font = pygame.font.SysFont('Arial', 40)
+		self.left_scr = 0
+		self.right_scr = 0
+
+		self.update()
+
+	def render(self):
+		self.screen.fill((0, 0, 0))
+		self.render_objs()
+		self.render_labels()
+		pygame.display.flip()
+
+	def ball_reset_pos(self):
+		self.ball.x = 400
+		self.ball.y = 300
+
+	def left_score(self):
+		self.left_scr += 1
+		self.ball_reset_pos()
+
+	def right_score(self):
+		self.right_scr += 1
+		self.ball_reset_pos()
+
+	def render_labels(self):
+		self.left_scr_label = self.font.render(str(self.left_scr), 0, (255, 255, 255))
+		self.right_scr_label = self.font.render(str(self.right_scr), 0, (255, 255, 255))
+		self.screen.blit(self.left_scr_label, (0+self.left_scr_label.get_width()*2, 50))
+		self.screen.blit(self.right_scr_label, (800-self.right_scr_label.get_width()*3, 50))
+
+	def render_objs(self):
+		self.ball.render(self.screen)
+		self.paddle_left.render(self.screen)
+		self.paddle_right.render(self.screen)
+
+	def update_objs(self):
+		self.ball.update()
+		self.paddle_left.update(self.ball)
+		self.paddle_right.update(self.ball)
+
+	def update(self):
+		while self.running:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT: sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_w:
+						self.paddle_left.move_up()
+					elif event.key == pygame.K_s:
+						self.paddle_left.move_down()
+				if event.type == pygame.KEYUP:
+					self.paddle_left.vy = 0
+				if event.type == pygame.USEREVENT+1:
+					self.left_score()
+				if event.type == pygame.USEREVENT+2:
+					self.right_score()
+			self.update_objs()
+			self.render()
--- objs.py
+++ objs.py
@@ -1,3 +1,5 @@
+import pygame
+
 # Base game object class
 class FGameObject():
 
@@ -7,18 +9,16 @@ class FGameObject():
 		self.y = 0
 		self.w = 0
 		self.h = 0
+		self.view = None
 
-		# Velocity, reset each frame
+		# Velocity, added to position each frame
 		self.vx = 0
 		self.vy = 0
 
 	def move(self):
 		# Add velocity to position
 		self.x = self.x + self.vx
-		self.y = self.y + self.xy
-
-		# Reset velocity
-		self.vx, self.vy = 0
+		self.y = self.y + self.vy
 
 	def update(self):
 		self.move()
@@ -27,13 +27,84 @@ class FGameObject():
 class FPaddle(FGameObject):
 
 	def __init__(self):
+		FGameObject.__init__(self)
 		self.w = 50
-		self.h = 100
+		self.h = 125
 
 	def set_player_left(self):
 		self.x = 50
-		self.y = 50
+		self.y = 250
 
 	def set_player_right(self):
 		self.x = 700
-		self.y = 50
+		self.y = 250
+
+	def move_up(self):
+		if self.y > 0:
+			self.vy = self.vy - 1
+
+	def move_down(self):
+		if self.y < 800:
+			self.vy = self.vy + 1
+
+	def move(self):
+		FGameObject.move(self)
+
+	def check_ball_collision(self, ball):
+		if ball.y >= self.y and ball.y <= self.y+self.h:
+			if ball.x >= self.x and ball.x <= self.x+self.w:
+				ball.reverse_direction_x()
+
+	def render(self, surface):
+		pygame.draw.rect(surface, (255, 255, 255), pygame.Rect(self.x, self.y, self.w, self.h))
+
+	def update(self, ball):
+		FGameObject.update(self)
+		self.check_ball_collision(ball)
+
+# Pong ball class
+class FBall(FGameObject):
+
+	def __init__(self):
+		# Width and height used for collision detection, radius for rendering
+		FGameObject.__init__(self)
+		self.x = 400
+		self.y = 300
+		self.w = 50
+		self.h = 50
+		self.r = 25
+		self.vx = 1
+		self.vy = 1
+
+	def reverse_direction_x(self):
+		self.vx = -self.vx
+
+	def reverse_direction_y(self):
+		self.vy = -self.vy
+
+	def collide_wall(self):
+		reverse_direction_y()
+
+	def collide_paddle(self, paddle):
+		calculate_direction(paddle.y)
+		reverse_direction_x()
+
+	# The ball's new direction is based off where the ball lands on the paddle
+	# If the ball lands directly in the center of the paddle, it moves horizontally
+	# If the ball lands on the edge, it bounces 75 degrees perpidicular to the paddle
+	def calculate_direction(self, paddle_pos_y):
+		pass
+
+	def render(self, surface):
+		pygame.draw.circle(surface, (255, 255, 255), (self.x, self.y), self.r)
+
+	def move(self):
+		FGameObject.move(self)
+		if self.x <= 0:
+			pygame.event.post(pygame.event.Event(pygame.USEREVENT+2))
+			self.reverse_direction_x()
+		elif self.x >= 800:
+			pygame.event.post(pygame.event.Event(pygame.USEREVENT+1))
+			self.reverse_direction_x()
+		if self.y <= 0 or self.y >= 600:
+			self.reverse_direction_y()
